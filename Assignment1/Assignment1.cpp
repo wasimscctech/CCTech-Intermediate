e5aaaecaabc4dda58925bf69f8e595950ba00067
Wasim_Shaikh <wasimahmad4210@gmail.com>
2022-12-01 03:54:25 +0530

    updated the cpp file

--- Assignment1/Assignment1.cpp
+++ Assignment1/Assignment1.cpp
@@ -4,103 +4,101 @@
 // #include "assignment1.h"
 #include <assert.h>
 #include <algorithm>
-#include <iostream>
 #include <list>
-#include <string>
-#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-    char s[] = "/hiker";
-    char slash = '/';
-    char *ptrslash = strrchr(s, slash);
+    char s[] = "hikerSpec.feature";
+    int size = sizeof(s) / sizeof(s[0]);
+    cout<<"size"<<size;
+    int *index1;
+    int *index2;
+    int zero = 0;
+    int indx1;
+    int indx2;
+    for (int i = 0; i < size - 1; i++)
+    {
+        if (s[i] == '/')
+        {
+            char *ptrslash = strrchr(s, '/');
+            indx1 = ptrslash - s;
+            index1 = &indx1;
+        }
+        else if (indx1 < 0 || indx1 >= size)
+        {
+            indx1 = zero;
+            index1 = &indx1;    
+        }
 
-    int index1 = ptrslash - s;
-    cout << index1 << endl;
+        else index1 = &zero; 
+        if (s[i] == '.')
+        {
+            char *ptrdot = strrchr(s, '.');
+            indx2 = ptrdot - s;
+            index2 = &indx2;
+        }
+        else if (indx2 < 0 || indx2 > size - 1)
+        {
+            indx2 = size - 1;
+            index2 = &indx2;
+        }
+    }
 
-    char dot = '.';
-    char *ptrdot = strrchr(s, dot);
+    cout << "index1: " << *index1;
+    cout << "index2: " << *index2;
 
-    int index2 = ptrdot - s;
-    cout << index2 << endl;
-
-    // string str(s + index1 + 1, s + index2);
-    string str(s, s + index2);
-    cout << str;
+    string str(s, s + *index2);
 
     list<string> listOfStrs = {
-        "steps_", "_steps","Steps_", "_Steps","steps.", ".steps","Steps.", ".Steps","steps-", "-steps","Steps-", "-Steps", "tests_", "_tests", "Tests_", "_Tests","tests.", ".tests", "Tests.", ".Tests","tests-", "-tests", "Tests-","-Tests","spec_", "_spec", "Spec_", "_Spec", "spec.",".spec", "Spec.", ".Spec", "spec-","-spec", "Spec-", "-Spec",
-        "test_", "_test", "Test_", "_Test","test.", ".test", "Test.", ".Test","test-", "-test", "Test-", "-Test",
-        "Tests", "tests", "steps", "Steps", "Spec", "spec", "Test", "test"
-        };
+        "steps_", "_steps", "Steps_", "_Steps", "steps.", ".steps", "Steps.", ".Steps", "steps-", "-steps", "Steps-", "-Steps", "tests_", "_tests", "Tests_", "_Tests", "tests.", ".tests", "Tests.", ".Tests", "tests-", "-tests", "Tests-", "-Tests", "spec_", "_spec", "Spec_", "_Spec", "spec.", ".spec", "Spec.", ".Spec", "spec-", "-spec", "Spec-", "-Spec",
+        "test_", "_test", "Test_", "_Test", "test.", ".test", "Test.", ".Test", "test-", "-test", "Test-", "-Test",
+        "Tests", "tests", "steps", "Steps", "Spec", "spec", "Test", "test"};
 
     list<string>::iterator it1, it2, it3;
-    // string str = "wibble/test/hiker_spec";
     int n = str.length();
-    cout<<n<<endl;
 
-    for (int i = index1; i <= n - 1; i++)
+    for (int i = *index1; i < n; i++)
     {
         it1 = find(listOfStrs.begin(), listOfStrs.end(), str.substr(i, 6));
         it2 = find(listOfStrs.begin(), listOfStrs.end(), str.substr(i, 5));
         it3 = find(listOfStrs.begin(), listOfStrs.end(), str.substr(i, 4));
 
         if (it1 != listOfStrs.end())
-        {   
-            cout<<str.substr(i, 6);
-            if (i == index1+1) cout << "[" << i + 6 << "," << n << "]";
-            else cout << "[" << index1+1 << "," << i << "]";
+        {
+            if (i == *index1 + 1 || i == *index1)
+                cout << "[" << i + 6 << "," << n << "]";
+            else if (str.substr(*index1, 1) != "/")
+                cout << "[" << *index1 << "," << i << "]";
+            else
+                cout << "[" << *index1 + 1 << "," << i << "]";
             break;
         }
         else if (it2 != listOfStrs.end())
-        {   
-            cout<<str.substr(i, 5);
-            if (i == index1+1) cout << "[" << i + 5 << "," << n << "]";
-            else cout << "[" << index1+1 << "," << i << "]";
+        {
+            if (i == *index1 + 1 || i == *index1)
+                cout << "[" << i + 5 << "," << n << "]";
+            // else if (str.substr(*index1,1) != "/")
+            //     cout << "[" << *index1 << "," << i << "]";
+            else
+                cout << "[" << *index1 + 1 << "," << i << "]";
             break;
         }
-        else if (it2 != listOfStrs.end())
-        {   
-            cout<<str.substr(i, 4);
-            if (i == index1+1) cout << "[" << i + 4 << "," << n << "]";
-            else cout << "[" << index1+1 << "," << i << "]";
+        else if (it3 != listOfStrs.end())
+        {
+            if (i == *index1 + 1 || i == *index1)
+                cout << "[" << i + 4 << "," << n << "]";
+            // else if (str.substr(*index1,1) != "/")
+            //     cout << "[" << *index1 << "," << i << "]";
+            else
+                cout << "[" << *index1 + 1 << "," << i << "]";
             break;
-        }
-        else {
-
         }
     }
 
-    // int n = str.length();
+    // if (s[*index1] != '/' && *index1 == 0 || *index2 == size-1)
+    //     cout << "[" << *index1 << "," << *index2 << "]";
 
-    // for (int i = 0; i <= n - 1; i++)
-    // {
-    //     // if (str.substr(i,5) == "tests" || str.substr(i,5) == "Tests" || str.substr(i,4) == "test" || str.substr(i,4) == "Test" || str.substr(i,4) == "spec" || str.substr(i,4) == "Spec")
-    //     if (str.substr(i, 5) == "tests" || str.substr(i, 5) == "Tests")
-    //     {
-    //         // cout<<i;
-    //         if (i == 0)
-    //         {
-    //             cout << "[" << i + 5 << "," << n << "]";
-    //         }
-    //         else
-    //         {
-    //             cout << "[" << 0 << "," << i << "]";
-    //         }
-    //     }
-    //     else if (str.substr(i, 4) == "test" || str.substr(i, 4) == "Test" || str.substr(i, 4) == "spec" || str.substr(i, 4) == "Spec")
-    //     {
-    //         if (i == 0)
-    //         {
-    //             cout << "[" << i + 4 << "," << n << "]";
-    //         }
-    //         else
-    //         {
-    //             cout << "[" << 0 << "," << i << "]";
-    //         }
-    //     }
-    // }
     return EXIT_SUCCESS;
 }
