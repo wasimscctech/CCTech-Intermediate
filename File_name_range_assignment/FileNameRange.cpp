5cb75df6f49dc1420fe95a0f1ba5742db6b73aa9
Wasim_Shaikh <wasimahmad4210@gmail.com>
2022-12-02 15:30:07 +0530

    refactored the code.

--- File_name_range_assignment/FileNameRange.cpp
+++ File_name_range_assignment/FileNameRange.cpp
@@ -1,9 +1,17 @@
 #include <stdlib.h>
 #include <cassert>
 #include "FileNameRange.h"
+#include <iostream>
 
 int main()
 {
-    fileRange("wibble/test/hiker_spec.rb");
+    fileNameRange obj1, obj2, obj3, obj4, obj5;
+    
+    assert(obj1.removeSlashAndDots("src/Hiker_spec.re")=="[4,9]");
+    assert(obj2.removeSlashAndDots("wibble/test/hiker_spec.rb")=="[12,17]");
+    assert(obj3.removeSlashAndDots("hiker_steps.rb")=="[0,5]");
+    assert(obj4.removeSlashAndDots("hiker_spec.rb")=="[0,5]");
+    assert(obj5.removeSlashAndDots("tests_hiker.sh")=="[6,11]");
+
     return EXIT_SUCCESS;
 }
--- File_name_range_assignment/FileNameRange.h
+++ File_name_range_assignment/FileNameRange.h
@@ -1,22 +1,32 @@
-#include <iostream>
+#include <sstream>
 #include <string.h>
+#include <stdlib.h>
 #include <assert.h>
 #include <algorithm>
 #include <list>
 
 using namespace std;
 
-void fileRange(string ipstr)
+class fileNameRange
 {
+public:
     int *index1;
     int *index2;
     int indx1;
     int indx2;
+    string result;
+    stringstream ss;
 
+    string removeSlashAndDots(string);
+    void searchStr(char *);
+};
+
+string fileNameRange::removeSlashAndDots(string ipstr)
+{
     char s[ipstr.length() + 1];
     strcpy(s, ipstr.c_str());
 
-    int size = sizeof(s) / sizeof(s[0]);
+    signed int size = sizeof(s) / sizeof(s[0]);
 
     for (int i = 0; i < size - 1; i++)
     {
@@ -26,7 +36,7 @@ void fileRange(string ipstr)
             indx1 = ptrslash - s;
             index1 = &indx1;
         }
-        else if (indx1 < 0 || indx1 >= size )
+        else if (indx1 < 0 || indx1 >= size)
         {
             indx1 = 0;
             index1 = &indx1;
@@ -44,50 +54,57 @@ void fileRange(string ipstr)
         }
     }
 
-    string str(s, s + *index2);
+    searchStr(s);
+    ss >> result;
+    return result;
+}
 
+void fileNameRange::searchStr(char s[])
+{
     list<string> listOfStrs = {
         "steps_", "_steps", "Steps_", "_Steps", "steps.", ".steps", "Steps.", ".Steps", "steps-", "-steps", "Steps-", "-Steps", "tests_", "_tests", "Tests_", "_Tests", "tests.", ".tests", "Tests.", ".Tests", "tests-", "-tests", "Tests-", "-Tests", "spec_", "_spec", "Spec_", "_Spec", "spec.", ".spec", "Spec.", ".Spec", "spec-", "-spec", "Spec-", "-Spec",
         "test_", "_test", "Test_", "_Test", "test.", ".test", "Test.", ".Test", "test-", "-test", "Test-", "-Test",
         "Tests", "tests", "steps", "Steps", "Spec", "spec", "Test", "test"};
 
     list<string>::iterator it1, it2, it3;
+
+    string str(s, s + *index2);
     int n = str.length();
 
-    for (int i = *index1; i < n ; i++)
+    for (int i = *index1; i < n; i++)
     {
         it1 = find(listOfStrs.begin(), listOfStrs.end(), str.substr(i, 6));
         it2 = find(listOfStrs.begin(), listOfStrs.end(), str.substr(i, 5));
         it3 = find(listOfStrs.begin(), listOfStrs.end(), str.substr(i, 4));
-    
+
         if (it1 != listOfStrs.end())
         {
-            if (i == *index1 + 1 || i == *index1 )
-                cout << "[" << i + 6 << "," << n << "]";
-            else if (str.substr(*index1,1) != "/")
-                cout << "[" << *index1  << "," << i << "]";
+            if (i == *index1 + 1 || i == *index1)
+                ss << "[" << i + 6 << "," << n << "]";
+
+            else if (str.substr(*index1, 1) != "/")
+                ss << "[" << *index1 << "," << i << "]";
+
             else
-                cout << "[" << *index1+1 << "," << i << "]";
+                ss << "[" << *index1 + 1 << "," << i << "]";
             break;
         }
         else if (it2 != listOfStrs.end())
         {
             if (i == *index1 + 1 || i == *index1)
-                cout << "[" << i + 5 << "," << n << "]";
+                ss << "[" << i + 5 << "," << n << "]";
             else
-                cout << "[" << *index1 + 1<< "," << i << "]";
+                ss << "[" << *index1 + 1 << "," << i << "]";
             break;
         }
         else if (it3 != listOfStrs.end())
         {
             if (i == *index1 + 1 || i == *index1)
-                cout << "[" << i + 4 << "," << n << "]";
+
+                ss << "[" << i + 4 << "," << n << "]";
             else
-                cout << "[" << *index1 + 1<< "," << i << "]";
+                ss << "[" << (*index1 + 1) << "," << i << "]";
             break;
         }
-        // else if (s[i] != '/' && it3 == listOfStrs.end() && it2 == listOfStrs.end() && it1 == listOfStrs.end()){
-        //     cout << "[" << 0 << "," << *index2 << "]";
-        // }
     }
 }
