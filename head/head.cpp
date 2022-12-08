1430ad4b77af83b032fd0b74a4eb263358b6a0f9
Wasim_Shaikh <wasimahmad4210@gmail.com>
2022-12-08 11:54:34 +0530

    updated the function name

--- head/head.cpp
+++ head/head.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
-#include <stdlib.h>
+// #include <iostream>
+// #include <stdlib.h>
 
-using namespace std;
+// using namespace std;
 
 int main(int argc, string argv[]) {
     if( argc == 1) {
@@ -11,25 +11,25 @@ int main(int argc, string argv[]) {
             c = getchar();
             if (c == EOF) break;
             putchar(c);
-        } 
+        }
     }
     return EXIT_SUCCESS;
 }
-    // else {
-    //     for (int i= 1; i<argc; i++) {
-    //         FILE *file;
-    //         if(file = fopen(argv[i], "r")) {
-    //             char c;
-    //             while(1) {
-    //                 c = getc(file);
-    //                 if (c == EOF)
-    //                     break;
-    //                 cout << c;
-    //             }
-    //             fclose(file);
-    //         }
-    //         else {
-    //             cout<<"cat: " << argv[i] << ": No such file or directory"<<endl;
-    //         }
-    //     }
-    // }
+// else {
+//     for (int i= 1; i<argc; i++) {
+//         FILE *file;
+//         if(file = fopen(argv[i], "r")) {
+//             char c;
+//             while(1) {
+//                 c = getc(file);
+//                 if (c == EOF)
+//                     break;
+//                 cout << c;
+//             }
+//             fclose(file);
+//         }
+//         else {
+//             cout<<"cat: " << argv[i] << ": No such file or directory"<<endl;
+//         }
+//     }
+// }
